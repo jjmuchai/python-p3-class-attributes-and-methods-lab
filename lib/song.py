14ae250b08e650d1ac882545c8413a04c0a52531
jjmuchai <jjmuchai151@gmail.com>
2025-05-24 12:36:49 +0300

    My updates to the project

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,30 @@
+#!/usr/bin/env python3
+
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update counters and lists
+        Song.count += 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+
+        # Count per genre
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Count per artist
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
